d32cbcffa4e55c979e38d46b15c96cc8787ff0ba
Maaarmotte <steam@marmottes-gmod.marmottes.net>
2016-01-31 13:13:06 +0000

    Updated structure

---
+++ lua/autorun/server/init.lua
@@ -0,0 +1,19 @@
+-- Send the files to clients
+AddCSLuaFile("autorun/client/cl_init.lua")
+
+for _,file in pairs(file.Find("mods/*.lua", "LUA")) do
+	AddCSLuaFile("mods/" .. file)
+end
+
+-- Run the files
+include("utils.lua")
+include("main.lua")
+include("hooks.lua")
+include("boat.lua")
+include("player.lua")
+include("derma.lua")
+include("mods.lua")
+
+for _,f in pairs(file.Find("mods/*.lua", "LUA")) do
+	include("mods/" .. f)
+end
