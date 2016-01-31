2dde770c48ed30fcef74d6287738308314094fb4
Maaarmotte <steam@marmottes-gmod.marmottes.net>
2016-01-31 15:02:34 +0000

    Added boost2

--- lua/hooks.lua
+++ lua/hooks.lua
@@ -1,9 +1,11 @@
+-- Command to enter the game
 hook.Add("PlayerSay", "Airboat", function(ply, text, isTeam)
 	if text == "!boat" then
 		AMMain.InitPlayer(ply)
 	end
 end)
 
+-- Main game ticks
 hook.Add("Tick", "Airboat", AMMain.Tick)
 
 hook.Add("CanPlayerEnterVehicle", "Airboat", function(ply, boat)
@@ -19,10 +21,21 @@ hook.Add("CanPlayerEnterVehicle", "Airboat", function(ply, boat)
 	end
 end)
 
+-- Stop the game when the player left or is killed
 hook.Add("PlayerLeaveVehicle", "Airboat", function(ply, boat)
 	local amPlayer = ply.AMPlayer
 	if amPlayer then
 		amPlayer:SetPlaying(false)
+	end
+end)
+
+-- Disable damages for player in airboat
+hook.Add("EntityTakeDamage", "Airboat", function(ply, dmg)
+	if not ply or not ply:IsValid() or not ply:IsPlayer() or not ply.AMPlayer then return end
+	local amPlayer = ply.AMPlayer
+
+	if amPlayer:GetPlaying() then
+		dmg:SetDamage(0)
 	end
 end)
 
