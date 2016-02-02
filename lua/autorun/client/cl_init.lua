bde601bee2d1472883621c36d8f06e7595397d04
Maaarmotte <steam@marmottes-gmod.marmottes.net>
2016-02-02 10:52:43 +0000

    Added very basic HUD

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -2,6 +2,9 @@
 include("utils.lua")
 include("derma.lua")
 include("mods.lua")
+include("cl_player.lua")
+include("cl_boat.lua")
+include("cl_hud.lua")
 
 for _,f in pairs(file.Find("mods/*.lua", "LUA")) do
 	include("mods/" .. f)
