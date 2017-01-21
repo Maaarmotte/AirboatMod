9ece10257ba5f00f5386a782d88244ee61692b55
Maaarmotte <loic.habegger@epfl.ch>
2017-01-22 00:31:02 +0100

    Added a skin, fixed particles not being rendered

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -9,3 +9,7 @@ include("cl_hud.lua")
 for _,f in pairs(file.Find("mods/*.lua", "LUA")) do
 	include("mods/" .. f)
 end
+
+-- Load the particles we need!
+game.AddParticles("particles/scary_ghost.pcf")
+game.AddParticles("particles/smoke_blackbillow.pcf")
